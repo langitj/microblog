09ceec5765662f6ce73ee4e7596245097774913b
J Langit <jerry.langit@gmail.com>
2020-05-29 14:14:03 +0800

    chapter 4-5

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+from datetime import timedelta
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
@@ -6,3 +8,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REMEMBER_COOKIE_DURATION = timedelta(minutes=30)
